feaa448d8baeec4893a86ba5207a013302bab75c
hippper <hippperxu@gmail.com>
2018-02-07 18:38:30 +0800

    add compile package script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm -rf output/bin
+rm -rf output/conf
+rm -rf output/log
+rm -rf frontEnd.zip
+
+mkdir -p output/bin
+mkdir -p output/conf
+mkdir -p output/log
+make
+
+cp load.sh output/load.sh
+
+cp bin/supervise.frontEnd output/bin/supervise.frontEnd
+cp bin/frontEnd output/bin/frontEnd || { echo error ; exit 1 ; }
+cp conf/frontEnd.toml output/conf/frontEnd.toml
+cp conf/log.json output/conf/log.json
+cp -r static output/
+cp -r templates output/
---
+++ load.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+cd `dirname $0` || exit
+ulimit -c unlimited
+
+start(){
+	stop
+	sleep 1
+	setsid /apps/frontEnd/bin/supervise.frontEnd env GOTRACEBACK=crash /apps/frontEnd -config /apps/frontEnd/conf/frontEnd.toml
+}
+
+stop(){
+    killall -9 supervise.frontEnd
+	killall -9 frontEnd
+}
+
+restart(){
+	stop
+    sleep 1
+    start
+}
+
+
+case C"$1" in
+	Cstart)
+		start
+		echo "start Done!"
+		;;
+	Cstop)
+		stop
+		echo "stop Done!"
+		;;
+	Crestart)
+		restart
+		echo "restart Done!"
+		;;
+	C*)
+		echo "Usage: $0 {start|stop|restart}"
+		;;
+esac
